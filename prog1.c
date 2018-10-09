c90d83f811b60c579c73f1515a5ed30457768721
pzxszbry <34458802+pzxszbry@users.noreply.github.com>
2018-10-09 01:28:44 -0500

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <sys/time.h>
+
+
+
+#define MatrixNumber 100
+int **a, **b,**c;
+
+
+void
+Matrix(int rows, int cols)
+{
+	int i, j;
+
+	a = (int**)malloc(sizeof(int*)*rows);
+	b = (int**)malloc(sizeof(int*)*rows);
+	c = (int**)malloc(sizeof(int*)*rows);
+
+	for(i = 0; i < rows; i++)
+	{
+		a[i]=(int*)malloc(sizeof(int)*cols);
+		b[i]=(int*)malloc(sizeof(int)*cols);
+		c[i]=(int*)malloc(sizeof(int)*cols);
+	}
+
+	for(i = 0;i < cols;i++)
+	{
+		for(j = 0; j < MatrixNumber;j++)
+		{
+			a[i][j] = rand()%2000+1;
+			b[i][j] = rand()%2000+1;
+		}
+	}
+}
+
+
+void 
+delay(int t)
+{
+	long micro = 0;
+
+	struct timeval tc,now;
+
+	gettimeofday(&now, NULL);
+
+	while(micro < t)
+	{
+		gettimeofday(&tc, NULL);
+		micro = tc.tv_sec*1000000L + tc.tv_usec - now.tv_sec*1000000L - now.tv_usec;
+	}
+
+}
+
+
+void 
+printMatrix(int **pArray, int rows, int cols)
+{
+	int i;
+	int j;
+	
+	for(i = 0; i < rows; i++)
+	{
+		for(j = 0; j < cols; j++)
+		{
+			printf("%d\t", pArray[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
+void 
+main()
+{
+	struct timeval start, finish;
+	int i, j, k;
+
+	srand(MatrixNumber);
+
+	Matrix(MatrixNumber, MatrixNumber);
+
+	gettimeofday(&start, NULL);
+	
+
+
+	
+	for(i = 0;i < MatrixNumber;i++)
+	{
+		for(j = 0; j < MatrixNumber;j++)
+		{
+			for(k = 0; k < MatrixNumber;k++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+			delay(10);
+		}
+	}
+
+	gettimeofday(&finish, NULL);
+
+	printf("Running Time: %d miliseconds\n", finish.tv_sec*1000 + finish.tv_usec/1000 - start.tv_sec*1000 - start.tv_usec/1000);
+	
+	
+	printf("%ld\t %ld\t %ld\t %ld\n",a[0][0], a[0][MatrixNumber-1], a[MatrixNumber-1][0], a[MatrixNumber-1][MatrixNumber-1]);
+	printf("%ld\t %ld\t %ld\t %ld\n",b[0][0], b[0][MatrixNumber-1], b[MatrixNumber-1][0], b[MatrixNumber-1][MatrixNumber-1]);
+	printf("%ld\t %ld\t %ld\t %ld\n",c[0][0], c[0][MatrixNumber-1], c[MatrixNumber-1][0], c[MatrixNumber-1][MatrixNumber-1]);
+	
+}
+
+
+
+
---
+++ prog2.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define MatrixNumber 1000
+#define NO_THREAD 25
+#define STEP (MatrixNumber/NO_THREAD)
+
+int **a, **b,**c;
+
+pthread_t tid[32];
+
+
+void
+Matrix(int rows, int cols)
+{
+	int i, j;
+
+	a = (int**)malloc(sizeof(int*)*rows);
+	b = (int**)malloc(sizeof(int*)*rows);
+	c = (int**)malloc(sizeof(int*)*rows);
+
+	for(i = 0; i < rows; i++)
+	{
+		a[i]=(int*)malloc(sizeof(int)*cols);
+		b[i]=(int*)malloc(sizeof(int)*cols);
+		c[i]=(int*)malloc(sizeof(int)*cols);
+	}
+
+	for(i = 0;i < cols;i++)
+	{
+		for(j = 0; j < MatrixNumber;j++)
+		{
+			a[i][j] = rand()%2000+1;
+			b[i][j] = rand()%2000+1;
+		}
+	}
+}
+
+
+void *
+thread_count (void *arg)
+{
+	int n = arg;
+	int head = (n * STEP);
+	int end = (head + STEP);
+	int i, j, k;
+	
+	for(i = head;i < end;i++)
+	{
+		for(j = 0; j < MatrixNumber;j++)
+		{
+			for(k = 0; k < MatrixNumber;k++)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+			delay(10);
+		}
+	}
+}
+
+void
+delay(int t)
+{
+	long micro = 0;
+
+	struct timeval tc,now;
+
+	gettimeofday(&now, NULL);
+
+	while(micro < t)
+	{
+		gettimeofday(&tc, NULL);
+		micro = tc.tv_sec*1000000L + tc.tv_usec - now.tv_sec*1000000L - now.tv_usec;
+	}
+
+}
+
+void 
+printMatrix(int **pArray, int rows, int cols)
+{
+	int i;
+	int j;
+	
+	for(i = 0; i < rows; i++)
+	{
+		for(j = 0; j < cols; j++)
+		{
+			printf("%d\t", pArray[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int
+main()
+{
+	int i,j;
+	
+	struct timeval start, finish;
+
+	srand(MatrixNumber);
+
+	Matrix(MatrixNumber, MatrixNumber);
+
+	gettimeofday(&start, NULL);
+
+	for( i = 0; i < NO_THREAD; i++ )
+		pthread_create( &tid[ i ], NULL, thread_count, (void *) i );
+
+	for( i = 0; i < NO_THREAD; i++ )
+		pthread_join( tid[ i ], NULL );
+
+	
+	gettimeofday(&finish, NULL);
+	
+	printf("Running Time: %d miliseconds\n", finish.tv_sec*1000 + finish.tv_usec/1000 - start.tv_sec*1000 - start.tv_usec/1000);
+	
+	printf("%ld\t %ld\t %ld\t %ld\n",a[0][0], a[0][MatrixNumber-1], a[MatrixNumber-1][0], a[MatrixNumber-1][MatrixNumber-1]);
+	printf("%ld\t %ld\t %ld\t %ld\n",b[0][0], b[0][MatrixNumber-1], b[MatrixNumber-1][0], b[MatrixNumber-1][MatrixNumber-1]);
+	printf("%ld\t %ld\t %ld\t %ld\n",c[0][0], c[0][MatrixNumber-1], c[MatrixNumber-1][0], c[MatrixNumber-1][MatrixNumber-1]);
+	
+	return 0;
+}
+
+
+
+
